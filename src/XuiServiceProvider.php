57738ce5cb8c0cd13ee3b4cf22118327e313bea3
Syahril Zulkefli <syahzul@gmail.com>
2020-09-12 01:06:18 +0800

    Initial commit

---
+++ src/XuiServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Xui;
+
+use Illuminate\Support\ServiceProvider;
+
+class XuiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'xui');
+
+        $this->loadViewComponentsAs('xui');
+    }
+}
